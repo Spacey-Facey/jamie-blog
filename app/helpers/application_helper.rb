0acc0e87f2e506b5d45ce6b5297ce6a3b9421125
Spacey-Facey <cadet-design@gmail.com>
2017-04-19 09:00:05 -0600

    add helpers and titles
    apply DRY methods and update test suite, title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return full title based on the current page
+  def full_title(page_title = "")
+    base_title = "Space Bubbles"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
