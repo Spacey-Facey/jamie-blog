075d856de2cd22a05a1dd7a85f6e734a61f29803
Spacey-Facey <cadet-design@gmail.com>
2017-04-27 13:20:14 -0600

    make the sass prettier
    and stuff

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,8 +1,8 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "sub-nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "sub-nav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
